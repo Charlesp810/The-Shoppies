9f9ad8d1395377579a54a42bf5da9e5ff65d07c7
Charlies Pan <Cp81089@gmail.com>
2021-01-11 17:44:56 -0500

    feat: access OMDB api from dispatched action

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from 'react'
+import { searchTitleThunk } from './reducers/movieReducer'
+import { useDispatch } from 'react-redux'
 
 function App() {
+  const [searchInput, setSearchInput] = useState('')
+  const dispatch = useDispatch()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(searchTitleThunk(searchInput))
+  }
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <input type="text" placeholder="Movie Title"></input>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Search Movie  Title" value={searchInput} onChange={(event) => setSearchInput(event.target.value)} />
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 }
--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,7 +1,33 @@
-const initialState = {}
+import axios from 'axios'
+
+//Action Type
+const SEARCH_TITLE = 'SEARCH_TITLE'
+//Action Creator
+export const searchTitle = (title) => ({
+  type: SEARCH_TITLE,
+  payload: title
+})
+//Thunk
+
+export const searchTitleThunk = (title) => {
+  return async dispatch => {
+    try {
+      const { data } = await axios.get(`http://www.omdbapi.com/?apikey=17fed5e6&type=movie&s=${title}`)
+      dispatch(searchTitle(data))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+const initialState = {
+  results: []
+}
 
 export default function movieReducer(state = initialState, action) {
   switch (action.type) {
+    case SEARCH_TITLE:
+      return { ...state, results: action.payload }
     default:
       return state;
   }
