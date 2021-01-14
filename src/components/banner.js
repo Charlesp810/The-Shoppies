c6c039d4aaff5f7978f814a51a502ead4461016e
Charlies Pan <Cp81089@gmail.com>
2021-01-14 15:56:38 -0500

    fixed styling and added automatic scroll to banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,6 +1,9 @@
 import goldenBanner from '../images/golden-award.png'
 
 function Banner() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  }
   return (
     <div className="banner-container">
       <img className="banner-image" src={goldenBanner} alt="golden banner" />
@@ -8,6 +11,7 @@ function Banner() {
         <h2>Nominations Are Set</h2>
         <h3>Thank You</h3>
       </div>
+      {scrollToTop()}
     </div>
   )
 }
